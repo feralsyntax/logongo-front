b35645c47f0e42c7cde1ee3538bcd6e664f78dfb
benie254 <davinci.monalissa@gmail.com>
2024-06-13 17:32:32 +0300

    update (admin-app): remove firebase configs

--- Admin/src/app/app.module.ts
+++ Admin/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
-import { ErrorsModule } from './modules/errors/errors.module'
+import { ErrorsModule } from './modules/errors/errors.module';
 import { AnnouncementsModule } from './modules/announcements/announcements.module';
 import { CardsModule } from './modules/cards/cards.module';
 import { LogsModule } from './modules/logs/logs.module';
@@ -22,30 +22,8 @@ import { HomeComponent } from './home/home.component';
 import * as Notiflix from 'notiflix';
 import { UsersModule } from './modules/users/users.module';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDeG0j_7ZNhPnglcYNbEMepis2CExqMxeg",
-  authDomain: "logongo.firebaseapp.com",
-  projectId: "logongo",
-  storageBucket: "logongo.appspot.com",
-  messagingSenderId: "353755377556",
-  appId: "1:353755377556:web:03e858b68e824a98e16981"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -65,8 +43,8 @@ const app = initializeApp(firebaseConfig);
     UsersModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS,  useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
